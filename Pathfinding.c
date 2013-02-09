880198a287f0948e5c1b58b054d92fc37f78c110
Ivica Gunjaca <ivgunjac@inet.hr>
2013-02-09 19:43:26 +0100

    V1.0

--- Pathfinding.c
+++ Pathfinding.c
@@ -1,31 +1,141 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <glib.h>
+
+/*
+ * Author: Ivica Gunjaca 
+ * use "gcc -Wall `pkg-config --libs --cflags glib-2.0` Pathfinding.c" to compile 
+ */
+
+/* Node represents a position in a 2D array */
+typedef struct node_t {
+  int x;
+  int y;
+} Node;
+
+/* MAX_X and MAX_Y are boundaries for the 2D array */
+#define MAX_X 10
+#define MAX_Y 10
+
+/* Globals */
+int world[MAX_X][MAX_Y];
+int visited[MAX_X][MAX_Y];
+
+/*
+ * Finds the shortest path between @start and @end Node
+ * using Breath First Search. Breath First Search can
+ * be used because links between nodes are unweighted.  
+ */
+void bfs(const Node* start, const Node* end){
+  GQueue* queue = g_queue_new();
+  Node* current_node = malloc(sizeof(Node));
+  (*current_node).x = (*start).x;
+  (*current_node).y = (*start).y;
+  int i, j;
+    
+  if (is_nodes_equal(start, end)){
+    printf("Start node equals end node");
+  }
+
+  visited[(*start).x][(*start).y] = 1;
+  g_queue_push_tail(queue, current_node);
+    
+  while (!g_queue_is_empty(queue)){
+    current_node = (Node*)g_queue_peek_head(queue);
+    g_queue_pop_head(queue);
+    
+    /* find neighbouring nodes, assume we can move diagonally */
+    for (i = (*current_node).x - 1; i <= (*current_node).x + 1; i++){
+      for (j = (*current_node).y - 1; j <= (*current_node).y + 1; j++){
+	/* check for world bounds */
+	if ((i < 0) || (j < 0) || (i >= MAX_X) || (j >= MAX_Y)){
+	  continue;
+	}
+
+	/* check for obstacles */
+	if (world[i][j] == 1){
+	  continue;
+	}
+	
+	/* check if already visited */
+	if (visited[i][j] > 0){
+	  continue;
+	}
+
+	Node* neighbour = malloc(sizeof(Node));
+	(*neighbour).x = i;
+	(*neighbour).y = j;
+	
+	visited[i][j] = visited[(*current_node).x][(*current_node).y] + 1;
+	if (is_nodes_equal(neighbour, end)){
+	  printf("End node reached!!! \n");
+	  return;
+	}
+	
+	g_queue_push_tail(queue, neighbour);
+      }
+    }
+    free(current_node);
+  }
+  
+  g_queue_free(queue);
+}
+
+/*
+ * Tests Nodes @first and @second for equality
+ */
+gboolean is_nodes_equal(const Node* first, const Node* second){
+  if (((*first).x == (*second).x) && ((*first).y == (*second).y)){
+    return TRUE;
+  } else {
+    return FALSE;
+  }
+}
 
 int main(){
-  printf("Pathfinding techniques\n");
-  int world[10][10];
-  // a cell in the world has a 25% probabilty to be an obstacle
-  int obstacle_probability=25,i,j,n;
+  printf("Pathfinding BFS\n");
+  
+  /* A cell in the world has a 25% probabilty to be an obstacle */
+  int obstacle_probability = 25;
+  int i,j,n;
 
   srand(time(NULL));
 
-  // populate world with random obstacles
-  for (i=0;i<10;i++){
-    for (j=0;j<10;j++){
+  /* populate world with random obstacles */
+  for (i = 0;i < MAX_X;i++){
+    for (j = 0;j < MAX_Y;j++){
       n = rand()%100;
       if (n < obstacle_probability) 
-	world[i][j]=1;
+	world[i][j] = 1;
       else
-	world[i][j]=0;
+	world[i][j] = 0;
     }
   }
 
-  // print world
-  for (i=0;i<10;i++){
-    for (j=0;j<10;j++){
+  Node start = {0,0};
+  Node end = {9,9};
+  
+  /* make sure start and end positions aren't obstacles */
+  world[start.x][start.y] = 0;
+  world[end.x][end.y] = 0;
+
+  /* print world */
+  for (i = 0;i < MAX_X;i++){
+    for (j = 0 ;j < MAX_Y;j++){
       printf("%d", world[i][j]);
-      if (j==9) printf("\n");
+      if (j == (MAX_Y - 1)) printf("\n");
     }
   }
-    
+
+  bfs(&start, &end);
+
+  /* print path */
+  printf("To read the path, backtrack from the highest number to the lowest...\n");
+  for (i = 0;i < MAX_X;i++){
+    for (j = 0 ;j < MAX_Y;j++){
+      printf(" %3d ", visited[i][j]);
+      if (j == (MAX_Y - 1)) printf("\n");
+    }
+  }
+  return 0;  
 } 
